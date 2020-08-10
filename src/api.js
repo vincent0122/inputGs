7ffcdc1b2542df70fe8dacffb2149db4f910a191
vincent0122 <38850727+vincent0122@users.noreply.github.com>
2020-08-10 21:32:06 +0900

    don'tknow

--- src/api.js
+++ src/api.js
@@ -42,22 +42,25 @@ function setArray(arr) {
   };
 }
 
-const item = setArray();
+const item = setArray([]);
 //클로저 함수 끝
 
 //클로저 함수 시작(for get record Id)
-function setArray2(arr2) {
+function setArray2(ids) {
   return {
-    get_arr2: function () {
-      return arr2;
+    get_id : function () {
+      return ids;
     },
-    set_arr2: function (_id) {
-      arr2.push(_id);
+    set_id : function (_ids) {
+      ids.push( _ids)   //왜 var로 선언을 하지 않지?
   }
 };
 }
 
+
+
 const getRecordId = setArray2([]);
+
 //클로저 함수 끝(for get record Id)
 
 
@@ -74,29 +77,49 @@ apiRouter.post("/air_content_input", (req, res) => {
   var wri = getName(wri);
   var wri = wri[0].name
 
+<<<<<<< HEAD
   base("testing").create({
+=======
+   base("testing").create({
+>>>>>>> 7070095bb3491f662c8455abc0ea3d697c5278b9
     날짜: date,
     거래처2: buyer,
     작성자: wri,
     내용: contents
+<<<<<<< HEAD
   }), function(err, record) {
+=======
+  }, function(err, record) {
+>>>>>>> 7070095bb3491f662c8455abc0ea3d697c5278b9
     if (err) {
       console.error(err);
       return;
     }
       console.log(record.getId()); 
       var record_id = record.getId();
+<<<<<<< HEAD
       getRecordId.set_arr2(record_id);
       console.log(getRecordId.get_arr2());
   };
   
+=======
+      getRecordId.set_id(record_id);
+      console.log(getRecordId.get_id());
+  });
+
+setTimeout(function(){
+  var k = getRecordId.get_id();
+  var kk = k[0];
+
+>>>>>>> 7070095bb3491f662c8455abc0ea3d697c5278b9
   const responseBody = {
     version: "2.0",
     template: {
       outputs: [
         {
           simpleText: {
-            text: "입력완료(사진첨부를 원하면 '사진첨부'를 클릭하세요)",
+            //text: "입력완료(사진첨부를 원하면 '사진첨부'를 클릭하세요~)",
+            text: kk
           },
         },
       ],
@@ -105,15 +128,23 @@ apiRouter.post("/air_content_input", (req, res) => {
 
   setTimeout(function(){
   res.status(200).send(responseBody);
+<<<<<<< HEAD
   },1000);
 });
+=======
+ 
+},1000);
+})
+>>>>>>> 7070095bb3491f662c8455abc0ea3d697c5278b9
 
 apiRouter.post("/air_pic_input", (req, res) => {
 
-  var buyer = JSON.stringify(req.body.action.detailParams.pic.origin); 
+
+  var k = getRecordId.get_id();
   
-  var k = getRecordId.get_arr2();
-  console.log(k);
+  setTimeout(function(){
+    console.log(k);
+  },3000);
 
   const responseBody = {
     version: "2.0",
