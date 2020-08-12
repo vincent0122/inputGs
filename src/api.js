fa17eb3325a615994a4621e9cd20a9054f9b4953
vincent0122 <38850727+vincent0122@users.noreply.github.com>
2020-08-12 14:51:14 +0900

    set 500

--- src/api.js
+++ src/api.js
@@ -97,6 +97,7 @@ apiRouter.post("/air_content_input", (req, res) => {
       
   });
   
+  setTimeout(function(){
   const responseBody = {
     version: "2.0",
     template: {
@@ -127,6 +128,8 @@ apiRouter.post("/air_content_input", (req, res) => {
 
   
   res.status(200).send(responseBody);
+  },500);
+  
 });
 
 apiRouter.post("/air_pic_input", (req, res) => {
