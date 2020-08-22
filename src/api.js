1ec75b163a3c7e2337534fb1862a6595be050543
orealstone <38850727+vincent0122@users.noreply.github.com>
2020-08-23 00:29:01 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -194,6 +194,67 @@ apiRouter.post("/air_pic_input", (req, res) => {
  },1500); 
 });
 
+apiRouter.post("/list_record", (req, res) => {
+  
+  var buyer = JSON.stringify(req.body.action.detailParams.customer.value); 
+  var buyer = buyer.replace(/\"/g, "");
+  listRecord = new Array;
+  
+  setTimeout(function(){
+    base('dataBase').select({
+      
+      filterByFormula: '{거래처}="' + buyer + "\""
+  }).eachPage(function page(records, fetchNextPage) {
+      records.forEach(function(record) {
+        
+        var aa = record.get("날짜");
+        var bb = record.get("내용");
+        var cc = record.get("Attachments");
+
+        if (cc != undefined){
+        var cc = cc.map(p => p.url);  
+        var cc = cc.join(" ");
+          }
+        else if(cc === undefined){
+          var cc = "";
+        }
+        //item.set_arr(record.get('날짜') +"  " + record.get('내용') + "  " + kk);
+        item.set_arr(aa + " " + bb + "  " + cc);
+       
+      //console.log(result);
+      });
+
+      item.sort_arr();
+      var result = item.get_arr();
+      result3 = result.join(" /////////////////////////////////////////////////////////////////////// ");
+        
+        fetchNextPage();
+  
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+  });   
+},500);  
+ 
+  setTimeout(function(){
+    const responseBody = {
+      version: "2.0",
+      template: {
+        outputs: [
+            {
+                "simpleText": {
+                    "text": result3  
+                   // "text": "123"
+                }
+            }
+        ]
+    }
+  }
+        
+  res.status(200).send(responseBody);
+  item.ini_arr();
+},1000); 
+});
+
 apiRouter.post("/checkId", (req, res) => {
 
    var x = JSON.stringify(req.body);
