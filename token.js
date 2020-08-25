e19851e00a7c91608db3c323999dfd65ddcab0a2
orealstone <38850727+vincent0122@users.noreply.github.com>
2020-08-25 16:55:47 +0900

    Delete token.js

--- token.js
+++
@@ -1,8 +0,0 @@
-module.exports =
-{
-    "access_token": "ya29.a0AfH6SMBm0Gp30-w6aDs5JoSuxheFNkZg4MjwNPj7d2UUH7BN7T8yRQpnGAQG1TYgOXZHDuWZEBTh9q7ZTOo66LoTJWpH2Z87FimBClukzohu1T3D38jN_0uUtyJnupmTO_IGbafS2O8-wwauYxRQsGjCubcgi_OyflI",
-    "refresh_token": "1//0erRg5CzdX3qtCgYIARAAGA4SNwF-L9Irqv0CQ1y_4R3xXZUwrBLFajVuM4bO5iy3DMiF140OlYkaaZDy7-kXOrDE8DhkR_L7UJw",
-    "scope": "https://www.googleapis.com/auth/spreadsheets",
-    "token_type": "Bearer",
-    "expiry_date": 1598260908685
-};
